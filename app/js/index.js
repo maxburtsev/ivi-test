caed2a277828d6649ae5d888060fdde11417af40
Max <37935643+maxburtsev@users.noreply.github.com>
2020-03-09 00:09:50 +0300

    second commit

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const wrapperDiamond = document.querySelector('.diamond');
 const diamond = document.querySelector('.diamond__shape');
+
+
+let heightDiamond = diamond.offsetHeight;
 let timeOut;
+
+
+
+
+
 
 function counter() {
 
@@ -17,20 +26,44 @@ function counter() {
         } else {
             diamondCount.textContent = i;
         }
+        
+        console.log(diamondCount.textContent)
     }
 
-    let t = setInterval(tick, 1000);
+    let t = setInterval(tick, 500);
 
     tick();
-
+    
 }
+
+
 
 diamond.addEventListener("mouseover", function (event) {
     timeOut = setTimeout(counter, 1000);
     document.body.style.background = "blue"
-})
+});
 
 diamond.addEventListener("mouseout", function (event) {
     document.body.style.background = "transparent";
     clearTimeout(timeOut);
-})
+});
+
+wrapperDiamond.style.height = heightDiamond + 'px';
+
+window.addEventListener('resize',function(){
+    function resizeHeight() { 
+        
+        let heightDiamond = diamond.offsetHeight;
+        wrapperDiamond.style.height = heightDiamond + 'px';
+        
+    }
+    
+    resizeHeight();
+    setTimeout(counter, 500);
+
+});
+
+
+window.addEventListener('load',function(){
+    setTimeout(counter, 500);
+});
